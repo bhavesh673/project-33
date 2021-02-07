1274e0e21205b7ef40da41bfb54c5573415c5c46
bhavesh673 <73343927+bhavesh673@users.noreply.github.com>
2021-02-07 19:07:13 +0530

    Add files via upload

---
+++ Rows.js
@@ -0,0 +1,22 @@
+class Rows{
+    constructor(x,y,width,height){
+        var options = {
+            isStatic : true,
+        }
+
+       // this.body = Bodies.rectangle(x,y,w,h,options);
+        this.w = w;
+        this.h = h;
+        this.color=color(random(0, 255), random(0, 255), random(0, 255));
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill(this.color);
+        rect(pos.x,pos.y,this.w,this.h);
+    }
+
+    
+
+};
